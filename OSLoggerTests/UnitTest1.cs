6d5beacb57c8de3afcdffa65994528e908a9f77b
Tim Reynolds <timgreynolds@icloud.com>
2024-05-29 19:35:20 -0400

    Re-factored to hide/encapsulate 'unsafe' code.
    Also added a Unit Test project and reorganized the filesystem layout.
    
     Changes to be committed:
    	modified:   .gitignore
    	deleted:    OSLogger.cs
    	modified:   OSLogger.sln
    	renamed:    Images/Logo.png -> OSLogger/Images/Logo.png
    	renamed:    Native.c -> OSLogger/Native.c
    	new file:   OSLogger/OSLogger.cs
    	renamed:    OSLogger.csproj -> OSLogger/OSLogger.csproj
    	renamed:    PACKAGEREADME.md -> OSLogger/PACKAGEREADME.md
    	renamed:    README.md -> OSLogger/README.MD
    	renamed:    Version.props -> OSLogger/Version.props
    	new file:   OSLoggerTests/OSLoggerTests.csproj
    	new file:   OSLoggerTests/UnitTest1.cs

---
+++ OSLoggerTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace OSLoggerTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
