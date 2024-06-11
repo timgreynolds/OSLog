d5c415bca4b7acc8855529d83ccfca1c4b23ba13
Tim Reynolds <timgreynolds@icloud.com>
2024-06-10 21:20:22 -0400

    Changes to be committed: 	modified:   OSLogger/OSLogger.cs 	modified:   OSLogger/OSLogger.csproj 	modified:   OSLoggerTests/OSLoggerTests.csproj 	deleted:    OSLoggerTests/UnitTest1.cs

---
+++ OSLoggerTests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace com.mahonkin.tim.logging.OSLoggerTests;
+
+public class Tests
+{
+    public static void Main(string[] args)  
+    {
+        IntPtr logPtr = OSLogger.Create(nameof(Program), nameof(Main));
+        Console.WriteLine(OSLogger.IsEnabled(logPtr, OSLogType.OS_LOG_TYPE_DEFAULT));
+        OSLogger.Log(logPtr, OSLogType.OS_LOG_TYPE_ERROR, "OSLogger Log(Error)");
+    }
+}
